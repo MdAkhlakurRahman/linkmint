d7c5266394e454bb3319a42b556f869aef379c50
MdAkhlakurRahman <coolakhlakur10@gmail.com>
2026-07-20 19:46:07 +0530

    feat(jpa): create ShortUrl entity

--- src/main/java/io/github/mdakhlakurrahman/linkmint/entity/ShortUrl.java
+++ src/main/java/io/github/mdakhlakurrahman/linkmint/entity/ShortUrl.java
@@ -1,10 +1,24 @@
 package io.github.mdakhlakurrahman.linkmint.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "short_urls")
 @Entity
 public class ShortUrl {
     @Id
-    int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,length = 2048)
+    private String originalUrl;
+    @Column(nullable = false,length = 20,unique = true)
+    private String shortCode;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
 }
