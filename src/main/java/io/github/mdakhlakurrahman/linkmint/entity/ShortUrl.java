5ddab8b783a88b684404ba23d1deaca9eff03829
Md Akhlakur Rahman <98209111+MdAkhlakurRahman@users.noreply.github.com>
2026-07-20 15:52:50 +0530

    feat(jpa): create ShortUrl entity

---
+++ src/main/java/io/github/mdakhlakurrahman/linkmint/entity/ShortUrl.java
@@ -0,0 +1,10 @@
+package io.github.mdakhlakurrahman.linkmint.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ShortUrl {
+    @Id
+    int id;
+}
